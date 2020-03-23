5d1cf8f73528c004e0791dcbcb809c5edc68f862
pkmohabir1 <pkmohabir@dons.usfca.edu>
2020-03-22 22:48:16 -0700

    Second Commit

--- networkComp0.cc
+++ networkComp0.cc
@@ -18,71 +18,90 @@ NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
 int
 main (int argc, char *argv[])
 {
-  //uint32_t nPackets = 1;
+
+  NS_LOG_INFO("Setting nPackets");
+  uint32_t nPackets = 1;
 
   CommandLine cmd;
   cmd.AddValue("nPackets", "Number of packets to echo", nPackets);
   cmd.Parse (argc, argv);
-  
+
   Time::SetResolution (Time::NS);
   LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
   LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
-  NS_LOG_INFO ("Creating Topology");
-  NodeContainer nodes;
-  nodes.Create (4);
+
+   NS_LOG_INFO ("Create nodes.");
+   NodeContainer nodes;
+   nodes.Create (4);
+
+   NS_LOG_INFO("Set node containter nodes 0 and 1");
+   NodeContainer n1 = NodeContainer (nodes.Get (0), nodes.Get (1));
+   NS_LOG_INFO("Set node containter nodes 1 and 2");
+   NodeContainer n2 = NodeContainer (nodes.Get (1), nodes.Get (2));
+   NS_LOG_INFO("Set node containter nodes 2 and 3");
+   NodeContainer n3 = NodeContainer (nodes.Get (2), nodes.Get (3));
+
 
   PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("5ms"));
   NetDeviceContainer devices;
-  devices = pointToPoint.Install (nodes.get(0), nodes.get(1));
+  devices = pointToPoint.Install (n1);
+
+
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("5ms"));
   NetDeviceContainer devices1;
-  devices1 = pointToPoint.Install (nodes.get(2), nodes.get(1));
+  devices1 = pointToPoint.Install (n2);
+
+
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("5ms"));
   NetDeviceContainer devices2;
-  devices2 = pointToPoint.Install (nodes.get(3), nodes.get(1));
+  devices2 = pointToPoint.Install (n3);
+
+
 
   InternetStackHelper stack;
   stack.Install (nodes);
 
   Ipv4AddressHelper address;
-  address.SetBase ("10.1.1.0", "255.255.255.0");
 
-  Ipv4InterfaceContainer interfaces = address.Assign (devices);
-  Ipv4InterfaceContainer interfaces = address.Assign (devices1);
-  Ipv4InterfaceContainer interfaces = address.Assign (devices2);
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer interfaces0 = address.Assign (devices);
+
+  address.SetBase ("10.1.2.0", "255.255.255.0");
+  Ipv4InterfaceContainer interfaces1 = address.Assign (devices1);
+
+  address.SetBase ("10.1.3.0", "255.255.255.0");
+  Ipv4InterfaceContainer interfaces2 = address.Assign (devices2);
 
   UdpEchoServerHelper echoServer (9);
-  UdpEchoServerHelper echoServer1 (10);
-  UdpEchoServerHelper echoServer2 (11);
 
-
-  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
+  ApplicationContainer serverApps = echoServer.Install (nodes.Get(3));
   serverApps.Start (Seconds (1.0));
   serverApps.Stop (Seconds (10.0));
 
-  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
+  UdpEchoClientHelper echoClient (interfaces0.GetAddress (0), 9);
   echoClient.SetAttribute ("MaxPackets", UintegerValue (nPackets));
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
   echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
-  UdpEchoClientHelper echoClient1 (interfaces.GetAddress (1), 10);
-  echoClient.SetAttribute ("MaxPackets", UintegerValue (nPackets));
-  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+  ApplicationContainer clientApps = echoClient.Install (nodes.Get(0));
+  clientApps.Start(Seconds (2.0));
+  clientApps.Stop(Seconds (7.0));
 
-  UdpEchoClientHelper echoClient2 (interfaces.GetAddress (1), 11);
-  echoClient.SetAttribute ("MaxPackets", UintegerValue (nPackets));
-  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
+  Simulator::Stop(Seconds (30));
 
-  ApplicationContainer clientApps = echoClient.Install (nodes.Get (0);
-  clientApps.Start (Seconds (2.0));
-  clientApps.Stop (Seconds (10.0));
+  NS_LOG_INFO ("Run Simulation.");
 
   Simulator::Run ();
   Simulator::Destroy ();
+
+  NS_LOG_INFO ("Done.");
   return 0;
 }
