bfecd146771104dfd5e59ed7a0ff4a1735e82fc3
gphendry <gphendry@dons.usfca.edu>
2020-03-24 11:56:04 -0700

    got 4 node echo server working

--- networkComp0.cc
+++ networkComp0.cc
@@ -35,12 +35,12 @@ main (int argc, char *argv[])
    NodeContainer nodes;
    nodes.Create (4);
 
-   NS_LOG_INFO("Set node containter nodes 0 and 1");
-   NodeContainer n1 = NodeContainer (nodes.Get (0), nodes.Get (1));
-   NS_LOG_INFO("Set node containter nodes 1 and 2");
-   NodeContainer n2 = NodeContainer (nodes.Get (1), nodes.Get (2));
-   NS_LOG_INFO("Set node containter nodes 2 and 3");
-   NodeContainer n3 = NodeContainer (nodes.Get (2), nodes.Get (3));
+  //  NS_LOG_INFO("Set node containter nodes 0 and 1");
+  //  NodeContainer n1 = NodeContainer (nodes.Get (0), nodes.Get (1));
+  //  NS_LOG_INFO("Set node containter nodes 1 and 2");
+  //  NodeContainer n2 = NodeContainer (nodes.Get (1), nodes.Get (2));
+  //  NS_LOG_INFO("Set node containter nodes 2 and 3");
+  //  NodeContainer n3 = NodeContainer (nodes.Get (2), nodes.Get (3));
 
 
   PointToPointHelper pointToPoint;
@@ -48,19 +48,22 @@ main (int argc, char *argv[])
   pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
   pointToPoint.SetChannelAttribute ("Delay", StringValue ("5ms"));
   NetDeviceContainer devices;
-  devices = pointToPoint.Install (n1);
+  devices = pointToPoint.Install (nodes.Get(0), nodes.Get(1));
 
 
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("5ms"));
+  PointToPointHelper pointToPoint2;
+
+  pointToPoint2.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
+  pointToPoint2.SetChannelAttribute ("Delay", StringValue ("5ms"));
   NetDeviceContainer devices1;
-  devices1 = pointToPoint.Install (n2);
+  devices1 = pointToPoint2.Install (nodes.Get(1), nodes.Get(2));
 
+  PointToPointHelper pointToPoint3;
 
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("5ms"));
+  pointToPoint3.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
+  pointToPoint3.SetChannelAttribute ("Delay", StringValue ("5ms"));
   NetDeviceContainer devices2;
-  devices2 = pointToPoint.Install (n3);
+  devices2 = pointToPoint3.Install (nodes.Get(2), nodes.Get(3));
 
 
 
@@ -78,13 +81,13 @@ main (int argc, char *argv[])
   address.SetBase ("10.1.3.0", "255.255.255.0");
   Ipv4InterfaceContainer interfaces2 = address.Assign (devices2);
 
-  UdpEchoServerHelper echoServer (9);
+  UdpEchoServerHelper echoServer (9999);
 
   ApplicationContainer serverApps = echoServer.Install (nodes.Get(3));
   serverApps.Start (Seconds (1.0));
   serverApps.Stop (Seconds (10.0));
 
-  UdpEchoClientHelper echoClient (interfaces0.GetAddress (0), 9);
+  UdpEchoClientHelper echoClient (interfaces2.GetAddress (1), 9999);
   echoClient.SetAttribute ("MaxPackets", UintegerValue (nPackets));
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
   echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
