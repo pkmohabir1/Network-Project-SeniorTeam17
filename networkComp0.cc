e0323cf99a22adc3dacd22bd17efd4f58d286a63
gphendry <gphendry@dons.usfca.edu>
2020-04-14 17:48:05 -0700

    added config file parsing, now sets numPackets, payload size and UDP dest port

--- networkComp0.cc
+++ networkComp0.cc
@@ -4,6 +4,12 @@
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <iterator>
+
 // Compression Network Topology
 //
 //       10.1.1.0           10.1.1.0
@@ -11,6 +17,40 @@
 //    point-to-point
 //
  
+void parseConfig(std::map<std::string, std::string> &testing_map, std::string configPath) {
+
+  std::string test = "./testing.ini";
+	std::ifstream inifile(configPath);
+
+	char c = '=';
+
+	std::string key;
+
+	std::string value;
+
+	if(inifile.is_open()){
+
+		std::string line;
+		while(getline(inifile, line)){	
+
+			size_t equal_pos = line.find(c);
+
+			key = line.substr(0, equal_pos);
+			value = line.substr(equal_pos + 1, line.length());
+			testing_map[key] = value;
+		}
+		std::map<std::string, std::string>::iterator itr;
+		for(itr = testing_map.begin(); itr != testing_map.end(); ++itr){
+			std::cout << itr->first << " = " << itr->second << '\n' << std::endl;
+		}
+
+	}else{
+
+		std::cerr << "Coud not open file" << "\n";
+	}
+
+}
+
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
@@ -19,8 +59,18 @@ int
 main (int argc, char *argv[])
 {
 
+  std::map<std::string, std::string> testingMap;
+
+  parseConfig(testingMap, "./testing.ini");
+
+  // int srcPort = std::stoi(testingMap.at("src_port_udp"), nullptr, 10);
+
+  int dstPort = std::stoi(testingMap.at("dst_port_udp"), nullptr, 10);
+
+  int payloadSize = std::stoi(testingMap.at("payload_sz"), nullptr, 10);
+
   NS_LOG_INFO("Setting nPackets");
-  uint32_t nPackets = 1;
+  uint32_t nPackets = std::stoi(testingMap.at("num_udp_packets"));
 
   CommandLine cmd;
   cmd.AddValue("nPackets", "Number of packets to echo", nPackets);
@@ -74,16 +124,16 @@ main (int argc, char *argv[])
   address.SetBase ("10.1.3.0", "255.255.255.0");
   Ipv4InterfaceContainer interfaces2 = address.Assign (devices2);
 
-  UdpEchoServerHelper echoServer (9999);
+  UdpEchoServerHelper echoServer (dstPort);
 
   ApplicationContainer serverApps = echoServer.Install (nodes.Get(3));
   serverApps.Start (Seconds (1.0));
   serverApps.Stop (Seconds (10.0));
 
-  UdpEchoClientHelper echoClient (interfaces2.GetAddress (1), 9999);
+  UdpEchoClientHelper echoClient (interfaces2.GetAddress (1), dstPort);
   echoClient.SetAttribute ("MaxPackets", UintegerValue (nPackets));
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (payloadSize));
 
   ApplicationContainer clientApps = echoClient.Install (nodes.Get(0));
   clientApps.Start(Seconds (2.0));
